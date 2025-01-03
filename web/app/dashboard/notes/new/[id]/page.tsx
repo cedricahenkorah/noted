14ccea288ba43964f607b1a994b6e7528017f66f
cedricahenkorah <cedricahenkorah@gmail.com>
2025-01-03 21:51:05 +0000

    feat: enhance NewNotePage with loading and error states for note fetching

--- web/app/dashboard/notes/new/[id]/page.tsx
+++ web/app/dashboard/notes/new/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { EditorContent } from "@/components/notes/editor-content";
 import { EditorHeader } from "@/components/notes/editor-header";
 import { fetchNote, saveNote } from "@/lib/notes";
+import { NotebookText } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
@@ -12,22 +13,27 @@ export default function NewNotePage() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [tags, setTags] = useState<string[]>([]);
-  const [id, setID] = useState<string>("");
+  const [id, setId] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [errorState, setErrorState] = useState<boolean>(false);
   const params = useParams<{ id: string }>();
 
   useEffect(() => {
     async function getNote() {
       await fetchNote({ id: params.id })
         .then((response) => {
-          setID(response?.data?._id ?? "");
           setTitle(response?.data?.title ?? "");
           setContent(response?.data?.content ?? "");
           setTags(response?.data?.tags ?? []);
+          setId(response?.data?._id ?? "");
+          setLoading(false);
+          setErrorState(false);
         })
         .catch((error) => {
           if (error instanceof Error) {
             toast.error(error.message);
           }
+          setErrorState(true);
         });
     }
 
@@ -61,6 +67,36 @@ export default function NewNotePage() {
       error: (error) => error?.message,
     });
   };
+
+  if (loading) {
+    return (
+      <div className="flex-1 flex-col items-center justify-center bg-background">
+        <div className="flex h-full justify-center items-center">
+          <div className="flex items-center gap-2 self-center font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <NotebookText className="size-4" />
+            </div>
+            Loading note...
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (errorState) {
+    return (
+      <div className="flex-1 flex-col items-center justify-center bg-background">
+        <div className="flex h-full justify-center items-center">
+          <div className="flex items-center gap-2 self-center font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <NotebookText className="size-4" />
+            </div>
+            Error loading note. Try again later.
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col h-screen bg-background">
